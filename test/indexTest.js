66a20c3b23bf167f739d2c6ce0240ad8d32cd096
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-07 11:22:59 +0300

    commit tree fa21e3f9a02ab4f0b7058374ada41107afd1c465

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,56 +1,56 @@
 describe('index.js', () => {
-  describe('currentUser', () => {
-    it('is defined', () => {
-      expect(currentUser, "The 'currentUser' variable must contain a string").to.be.a('string');
-      expect(currentUser, "You need to modify the value of the 'currentUser' variable").to.not.be.empty;
-    });
-  });
-
-  describe('welcomeMessage', () => {
-    it('contains "Welcome to Flatbook, "', () => {
-      expect(welcomeMessage).to.have.string('Welcome to Flatbook, ');
+    describe('currentUser', () => {
+        it('is defzined', () => {
+            expect(currentUser, "The 'currentUser' variable must contain a string").to.be.a('string');
+            expect(currentUser, "You need to modify the value of the 'currentUser' variable").to.not.be.empty;
+        });
     });
 
-    it("contains the value of the 'currentUser' variable", () => {
-      expect(welcomeMessage).to.have.string(currentUser);
+    describe('welcomeMessage', () => {
+        it('contains "Welcome to Flatbook, "', () => {
+            expect(welcomeMessage).to.have.string('Welcome to Flatbook, ');
+        });
+
+        it("contains the value of the 'currentUser' variable", () => {
+            expect(welcomeMessage).to.have.string(currentUser);
+        });
+
+        it('ends with an exclamation point!', () => {
+            expect(welcomeMessage.substr(-1)).to.eq('!');
+        });
     });
 
-    it('ends with an exclamation point!', () => {
-      expect(welcomeMessage.substr(-1)).to.eq('!');
-    });
-  });
+    describe('excitedWelcomeMessage', () => {
+        it('contains "WELCOME TO FLATBOOK, "', () => {
+            expect(excitedWelcomeMessage).to.have.string('WELCOME TO FLATBOOK, ');
+        });
 
-  describe('excitedWelcomeMessage', () => {
-    it('contains "WELCOME TO FLATBOOK, "', () => {
-      expect(excitedWelcomeMessage).to.have.string('WELCOME TO FLATBOOK, ');
-    });
+        it("contains the value of the 'currentUser' variable", () => {
+            const upperCaseCurrentUser = currentUser.toUpperCase();
 
-    it("contains the value of the 'currentUser' variable", () => {
-      const upperCaseCurrentUser = currentUser.toUpperCase();
+            expect(excitedWelcomeMessage).to.have.string(upperCaseCurrentUser);
+        });
 
-      expect(excitedWelcomeMessage).to.have.string(upperCaseCurrentUser);
-    });
-
-    it('ends with an exclamation point', () => {
-      expect(excitedWelcomeMessage.substr(-1)).to.eq('!');
-    });
-  });
-
-  describe('shortGreeting', () => {
-    it(`contains "Welcome, "`, () => {
-      expect(shortGreeting).to.have.string('Welcome, ');
+        it('ends with an exclamation point', () => {
+            expect(excitedWelcomeMessage.substr(-1)).to.eq('!');
+        });
     });
 
-    it("contains the first initial of the name stored in the 'currentUser' variable", () => {
-      const firstInitial = currentUser[0];
-      const restOfName = currentUser.slice(1);
+    describe('shortGreeting', () => {
+        it(`contains "Welcome, "`, () => {
+            expect(shortGreeting).to.have.string('Welcome, ');
+        });
 
-      expect(shortGreeting).to.have.string(firstInitial);
-      expect(shortGreeting).to.not.have.string(restOfName);
-    });
+        it("contains the first initial of the name stored in the 'currentUser' variable", () => {
+            const firstInitial = currentUser[0];
+            const restOfName = currentUser.slice(1);
 
-    it('ends with an exclamation point', () => {
-      expect(shortGreeting.substr(-1)).to.eq('!');
+            expect(shortGreeting).to.have.string(firstInitial);
+            expect(shortGreeting).to.not.have.string(restOfName);
+        });
+
+        it('ends with an exclamation point', () => {
+            expect(shortGreeting.substr(-1)).to.eq('!');
+        });
     });
-  });
 });
